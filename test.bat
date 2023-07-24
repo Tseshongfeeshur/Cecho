8e61c859cbf5532b181803ad0af4a9752af581af
Ryan璃黯 <99335871+Tseshongfeeshur@users.noreply.github.com>
2023-07-24 21:58:31 +0800

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,40 @@
+@echo off
+
+cecho
+
+echo.
+
+cecho {00}00{10}10{20}20{30}30{40}40{50}50{60}60{70}70{80}80{90}90{A0}A0{B0}B0{C0}C0{D0}D0{E0}E0{F0}F0{#}{\n}
+cecho {01}01{11}11{21}21{31}31{41}41{51}51{61}61{71}71{81}81{91}91{A1}A1{B1}B1{C1}C1{D1}D1{E1}E1{F1}F1{#}{\n}
+cecho {02}02{12}12{22}22{32}32{42}42{52}52{62}62{72}72{82}82{92}92{A2}A2{B2}B2{C2}C2{D2}D2{E2}E2{F2}F2{#}{\n}
+cecho {03}03{13}13{23}23{33}33{43}43{53}53{63}63{73}73{83}83{93}93{A3}A3{B3}B3{C3}C3{D3}D3{E3}E3{F3}F3{#}{\n}
+cecho {04}04{14}14{24}24{34}34{44}44{54}54{64}64{74}74{84}84{94}94{A4}A4{B4}B4{C4}C4{D4}D4{E4}E4{F4}F4{#}{\n}
+cecho {05}05{15}15{25}25{35}35{45}45{55}55{65}65{75}75{85}85{95}95{A5}A5{B5}B5{C5}C5{D5}D5{E5}E5{F5}F5{#}{\n}
+cecho {06}06{16}16{26}26{36}36{46}46{56}56{66}66{76}76{86}86{96}96{A6}A6{B6}B6{C6}C6{D6}D6{E6}E6{F6}F6{#}{\n}
+cecho {07}07{17}17{27}27{37}37{47}47{57}57{67}67{77}77{87}87{97}97{A7}A7{B7}B7{C7}C7{D7}D7{E7}E7{F7}F7{#}{\n}
+cecho {08}08{18}18{28}28{38}38{48}48{58}58{68}68{78}78{88}88{98}98{A8}A8{B8}B8{C8}C8{D8}D8{E8}E8{F8}F8{#}{\n}
+cecho {09}09{19}19{29}29{39}39{49}49{59}59{69}69{79}79{89}89{99}99{A9}A9{B9}B9{C9}C9{D9}D9{E9}E9{F9}F9{#}{\n}
+cecho {0A}0A{1A}1A{2A}2A{3A}3A{4A}4A{5A}5A{6A}6A{7A}7A{8A}8A{9A}9A{AA}AA{BA}BA{CA}CA{DA}DA{EA}EA{FA}FA{#}{\n}
+cecho {0B}0B{1B}1B{2B}2B{3B}3B{4B}4B{5B}5B{6B}6B{7B}7B{8B}8B{9B}9B{AB}AB{BB}BB{CB}CB{DB}DB{EB}EB{FB}FB{#}{\n}
+cecho {0C}0C{1C}1C{2C}2C{3C}3C{4C}4C{5C}5C{6C}6C{7C}7C{8C}8C{9C}9C{AC}AC{BC}BC{CC}CC{DC}DC{EC}EC{FC}FC{#}{\n}
+cecho {0D}0D{1D}1D{2D}2D{3D}3D{4D}4D{5D}5D{6D}6D{7D}7D{8D}8D{9D}9D{AD}AD{BD}BD{CD}CD{DD}DD{ED}ED{FD}FD{#}{\n}
+cecho {0E}0E{1E}1E{2E}2E{3E}3E{4E}4E{5E}5E{6E}6E{7E}7E{8E}8E{9E}9E{AE}AE{BE}BE{CE}CE{DE}DE{EE}EE{FE}FE{#}{\n}
+cecho {0F}0F{1F}1F{2F}2F{3F}3F{4F}4F{5F}5F{6F}6F{7F}7F{8F}8F{9F}9F{AF}AF{BF}BF{CF}CF{DF}DF{EF}EF{FF}FF{#}{\n}
+
+echo.
+
+cecho {\t}Tab
+echo.
+cecho Unicode {\u793a}{\u4f8b}
+echo.
+cecho Escape charater {{
+echo.
+cecho {green}green{default} and back to initial color
+echo.
+cecho {a red foreground on a white background}a red foreground on a white background{# #} and back to initial colors
+echo.
+cecho {blue gray}blue gray{#} and back to initial colors
+echo.
+cecho {purple yellow}purple yellow{#} and back to initial colors
+
+pause>nul
